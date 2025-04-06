0ce3180fa4bf33ca7cdc27dbbd4d1f49390c0649
reavscripts <72871071+reavscripts@users.noreply.github.com>
2025-04-06 15:55:33 +0200

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -2,8 +2,8 @@ local function getScriptUrl()
     -- Aggiungi qui gli URL degli script in base al PlaceId e GameId
     local scripts = {
         [7074860883] = {  -- Esegui solo se il GameId è uguale a questo
-            [128336380114944] = "https://raw.githubusercontent.com/reavscripts/arise/refs/heads/main/main.lua",  -- Script per il Dungeon
-            default = "https://raw.githubusercontent.com/reavscripts/arise/refs/heads/main/main.lua"  -- Script predefinito
+            [128336380114944] = "",  -- Script per il Dungeon
+            default = "https://raw.githubusercontent.com/reavscripts/arise/refs/heads/main/default.lua"  -- Script predefinito
         },
         -- Aggiungi altri giochi qui
         [12345678901234] = {  -- Un altro esempio di gioco
@@ -19,7 +19,9 @@ local function getScriptUrl()
         -- Se la situazione è soddisfatta, carica un URL diverso
         -- Ad esempio, aggiungiamo uno script specifico per questa situazione
         showMessage("Special script triggered due to Room_1 and Portal.", 3)
-        return "https://raw.githubusercontent.com/reavscripts/special_script_for_room1.lua"
+        return "https://raw.githubusercontent.com/reavscripts/arise/refs/heads/main/infernal.lua"
+    else
+        return "https://raw.githubusercontent.com/reavscripts/arise/refs/heads/main/dungeon.lua"
     end
 
     -- Restituisci l'URL giusto in base al GameId e PlaceId
